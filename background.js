5343ef71b38906c6f93d3dce0204117db6e2237b
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-03-24 22:14:03 +0100

    fix: remove tabId usage, which did not work

--- background.js
+++ background.js
@@ -7,12 +7,11 @@
 	};
 	const extraInfoSpec = ["blocking"];
 
-	let tabIds_to_block_imgs_from = {} ;
 	let store = await browser.storage.local.get(extId);
 	store = (typeof store[extId] !== 'undefined') ? store[extId] : {} ;
 	//console.log('store ', JSON.stringify(store,null,4));
 	let mode = (typeof store['mode'] === 'boolean') ? store['mode'] : false; // false := whitelist
-	
+
 	browser.browserAction.setTitle({title: (mode?"black":"white") + "list" });
 	browser.browserAction.setIcon({path: (mode?"black":"white") + ".png" });
 
@@ -20,7 +19,7 @@
 		await browser.storage.local.set({'dontLoadImages': store});
 	}
 
-	const BAonClicked = async () => {
+	async function BAonClicked() {
 		mode=(!mode);
 		browser.browserAction.setIcon({path: (mode?"black":"white") + ".png" });
 		browser.browserAction.setTitle({title: (mode?"black":"white") + "list" });
@@ -30,46 +29,52 @@
 
 		const tabs = await browser.tabs.query({});
 		for(let tab of tabs) {
-			browser.tabs.reload(tab.id);
-		}
-	};
-
-	const onBeforeRequest = (details) => {
-		if(mode) { // blacklist  
-			if (typeof tabIds_to_block_imgs_from[details.tabId] === 'boolean' && tabIds_to_block_imgs_from[details.tabId] === true) {
-				return {'cancel': true};
-			}
-		}else {  // whitelist 
-			if (typeof tabIds_to_block_imgs_from[details.tabId] !== 'boolean' ) {
-				return {'cancel': true};
-			}
-		}
-		return {'cancel': false};
-	};
-
-
-	async function PAonClicked(tab) {
-		const domain = new URL(tab.url);
-
-		if (typeof store[domain.origin] === 'undefined') {
-			store[domain.origin] = true;
-		}else{
-			store[domain.origin] = undefined;
-			delete store[domain.origin];
-		}
-		await setStorage(store);
-		const tabs = await browser.tabs.query({url: domain.origin + "/*"});
-		for(let tab of tabs) {
 			await browser.tabs.reload(tab.id);
 		}
 	};
 
-	async function onCompleted(details) {
+	function onBeforeRequest (details) {
+
+		const domain = new URL(details.originUrl);
+		const origin = domain.origin;
+
+		if(mode) { // blacklist 
+			if(typeof store[origin] === 'boolean'){
+				//console.log('blocked', details.url);
+				return {cancel: true};
+			}
+		}else{ // whitelist
+			if(typeof store[origin] !== 'boolean'){
+				//console.log('blocked', details.url);
+				return {cancel: true};
+			}
+		}
+		//console.log('loaded', details.url);
+	}
+
+
+	function PAonClicked(tab) {
+		const domain = new URL(tab.url);
+
+		if (typeof store[domain.origin] === 'boolean') {
+			delete store[domain.origin];
+		}else{
+			store[domain.origin] = true;
+		}
+		//console.log(store);
+		setStorage(store);
+		const tabs = await browser.tabs.query({url: domain.origin + "/*"});
+		for(let tab of tabs) {
+			browser.tabs.reload(tab.id);
+		}
+	};
+
+	function onCompleted(details) {
 		if (details.frameId !== 0) {
 			return;
 		}
 		const domain = new URL(details.url);
-		if (typeof store[domain.origin] === 'undefined') {
+		if (typeof store[domain.origin] !== 'boolean') {
 			browser.pageAction.setIcon({tabId: details.tabId, path: "plus.png" });
 			browser.pageAction.setTitle({tabId: details.tabId, title: "add to list" });
 		}else {
@@ -78,21 +83,8 @@
 		}
 	};
 
-	async function onBeforeNavigate(details) {
-		if (details.frameId !== 0) {
-			return;
-		}
-		const domain = new URL(details.url);
-		if (typeof store[domain.origin] === 'undefined') {
-			tabIds_to_block_imgs_from[details.tabId] = undefined;
-			delete tabIds_to_block_imgs_from[details.tabId]; 
-		}else {
-			tabIds_to_block_imgs_from[details.tabId] = true; 
-		}
-	};
 
 	browser.webNavigation.onCompleted.addListener(onCompleted);
-	browser.webNavigation.onBeforeNavigate.addListener(onBeforeNavigate);
 	browser.webRequest.onBeforeRequest.addListener(onBeforeRequest,filter,extraInfoSpec);
 	browser.browserAction.onClicked.addListener(BAonClicked);
 	browser.pageAction.onClicked.addListener(PAonClicked);
